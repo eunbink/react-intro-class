ce966bd13bd472d06c5b051a8e4396aef8071971
eunbink <kangeb1992@gmail.com>
2017-09-10 12:12:04 -0600

    calculator

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Calculator from './components/Calculator'
+
+import Calculator from './components/Calculator';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <Calculator/>
+      <div>
+        <Calculator />
       </div>
     );
   }
